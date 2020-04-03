42f115c4bec4ede57a4e8943258fb0c030a5ec0e
Ali BRIGUI <ali.brigui@esprit.tn>
2020-04-03 16:46:44 +0100

    sprint 0

--- src/tn/esprit/tgt/test/test.java
+++ src/tn/esprit/tgt/test/test.java
@@ -11,6 +11,7 @@ import tn.esprit.tgt.entities.*;
 import tn.esprit.tgt.services.*;
 
 
+
 /**
  *
  * @author ADMIN
@@ -22,27 +23,35 @@ public class test {
      */
     public static void main(String[] args) throws SQLException {
         // TODO code application logic here
-//        User u = new User(6, "kk", "kk", "12kk", "kk", "kk", "kk", "kk", 200145, "kk");
-////        UserService su = new UserService();
-////        su.ajouterUser(u);
-//        
-//        Categorie ct= new Categorie(4,"test");
-//        SousCategorie sct= new SousCategorie(5,"SousTest", ct);
-//        Sponsor s = new Sponsor(4,"Razer", "10 RUE PARIS", 74888555, "RAZER-CORP@GMAIL.COM", "Razer-corp.jpeg");
-//        Agence ag = new Agence(8,"Slim-corp","0000AB", "Slim-corp@gmail.com", "Ben Arous", 74888555, 74222333, "SlimCorp.com", "SlimCorp.jpeg", s, u);
-//        Date d = new Date();
-//        Evenement e = new Evenement("CoronaFest", "Drink Corona", d, "Munich" , d , 1000, sct, "Corona.jpeg", ag);
-//        EvenementService es =new EvenementService();
-//        es.ajouterEvenement(e);
+        User u = new User(6, "kk", "kk", "12kk", "kk", "kk", "kk", "kk", 200145, "kk");
+        UserService su = new UserService();
+        su.ajouterUser(u);
         
-//        java.util.Date d = new java.util.Date();
-//        java.sql.Date date = new java.sql.Date(d.getTime());
-//        Publication p = new Publication(11,"ddddeeee", u, date, "ddd.png");
-//        PublicationService ps = new PublicationService();
-//        ps.ajoutPublication(p);
-//        Commentaire c = new Commentaire("dsdsd", date, p, u);
-//        CommentaireService cs = new CommentaireService();
-//        cs.ajoutCommentaire(c);
+        Categorie ct= new Categorie(4,"test");
+        CategorieService kk = new CategorieService();
+        kk.ajouterCategorie(ct);
+        SousCategorie sct= new SousCategorie(5,"SousTest", ct);
+        SousCategorieService kkk = new SousCategorieService();
+        kkk.ajouterSousCategorie(sct);
+        Sponsor s = new Sponsor(4,"Razer", "10 RUE PARIS", 74888555, "RAZER-CORP@GMAIL.COM", "Razer-corp.jpeg");
+        SponsorService sk = new SponsorService();
+        sk.ajouterSponsor(s);
+        Agence ag = new Agence(8,"Slim-corp","0000AB", "Slim-corp@gmail.com", "Ben Arous", 74888555, 74222333, "SlimCorp.com", "SlimCorp.jpeg", s, u);
+        AgenceService nj = new AgenceService();
+        nj.ajouterAgence(ag);
+        Date d = new Date(0);
+        Evenement e = new Evenement("CoronaFest", "Drink Corona", d, "Munich" , d , 1000, sct, "Corona.jpeg", ag);
+        EvenementService es =new EvenementService();
+        es.ajouterEvenement(e);
+        
+        
+        java.sql.Date date = new java.sql.Date(d.getTime());
+        Publication p = new Publication(11,"ddddeeee", u, date, "ddd.png");
+        PublicationService ps = new PublicationService();
+        ps.ajoutPublication(p);
+        Commentaire c = new Commentaire("dsdsd", date, p, u);
+        CommentaireService cs = new CommentaireService();
+        cs.ajoutCommentaire(c);
         
         
     }
