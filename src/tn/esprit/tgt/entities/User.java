fc085c5a754b80920f5510488fe3e214fbff6c77
Ali BRIGUI <ali.brigui@esprit.tn>
2020-04-03 13:09:33 +0100

    Interface Login

--- src/tn/esprit/tgt/entities/User.java
+++ src/tn/esprit/tgt/entities/User.java
@@ -11,4 +11,129 @@ package tn.esprit.tgt.entities;
  */
 public class User {
     
+    private int id;
+    private String username;
+    private String email;
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", username=" + username + ", email=" + email + ", nom=" + nom + ", prenom=" + prenom + ", adresse=" + adresse + ", telephone=" + telephone + ", genre=" + genre + '}'+"\n";
+    }
+    String password;
+    private String roles;
+    private String nom;
+    private String prenom;
+    private String adresse;
+    private int telephone;
+    private String genre;
+
+    public User(int id, String username, String email, String password, String roles, String nom, String prenom, String adresse, int telephone, String genre) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.adresse = adresse;
+        this.telephone = telephone;
+        this.genre = genre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public int getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(int telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public User() {
+    }
+
+    
+      public User(int id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public User(int id) {
+        this.id = id;
+    }
+    
+    
 }
+
+
